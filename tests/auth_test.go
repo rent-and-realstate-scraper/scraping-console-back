c8f7a39f58f791079ee5057a342e12c4bcfa409a
Hugo J. Bello <hugojose@azlo.com>
2019-04-04 19:01:30 +0200

    fix

---
+++ tests/auth_test.go
@@ -0,0 +1,21 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	m "scraping-console-back"
+)
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	m.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
